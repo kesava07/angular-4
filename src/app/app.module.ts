c43c8491f0fc42016ac17e4104417f69038f731c
kesava07 <kesava8148420073@gmail.com>
2019-07-23 06:15:18 -0700

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
+import { CourseService } from './course.service';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CourseComponent } from './course/course.component';
+import { SummeryPipe } from './summery.pipe';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CourseComponent,
+    SummeryPipe
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [
+    CourseService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/course.service.ts
@@ -0,0 +1,5 @@
+export class CourseService {
+    getCourses() {
+        return ["Angular", "React JS", "VUE JS", "React Native"]
+    }
+}
